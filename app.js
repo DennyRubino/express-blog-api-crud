e46244db781f0c02d28db9479cc83e792b4305fb
Denny Rubino <rubidenny@gmail.com>
2025-01-22 23:46:38 +0100

    add data

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
-    const express = require('express') 
-    const app = express()
-    const port = 3000;
-    const postsRouter = require("./router/posts")
+const express = require("express");
+const app = express();
+const port = 3000;
+const postsRouter = require("./controllers/controllerPosts");
+const posts = require("./data/data");
 
+app.get("/", (req, res) => {
+  res.json(posts);
+});
 
-    app.get('/', (req, res) => {
-    res.send('Server del mio blog')
-    })
+app.use(express.static("public"));
 
-    app.use(express.static("public"))
+app.use("/posts", postsRouter);
 
-    app.use("/", postsRouter)
+app.get("/bacheca", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Ciambellone",
+      image: `http://localhost:3000/cibi/ciambellone.jpeg`,
+      taste_scale: "7/10",
+      tags: ["Dolci", "Torte", "Ricette vegetariane", "Ricette al forno"],
+    },
+    {
+      id: 2,
+      name: "cracker barbabietola",
+      image: `http://localhost:3000/cibi/cracker_barbabietola.jpeg`,
+      taste_scale: "6/10",
+      tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
+    },
+    {
+      id: 3,
+      name: "pane fritto dolce",
+      image: `http://localhost:3000/cibi/pane_fritto_dolce.jpeg`,
+      taste_scale: "9/10",
+      tags: ["Dolci", "Dolci veloci", "Ricette veloci", "Ricette vegetariane"],
+    },
+    {
+      id: 4,
+      name: "pasta barbabietola",
+      image: `http://localhost:3000/cibi/pasta_barbabietola.jpeg`,
+      taste_scale: "8/10",
+      tags: ["Primi piatti", "Ricette vegetariane"],
+    },
+    {
+      id: 5,
+      name: "torta paesana",
+      image: `http://localhost:3000/cibi/torta_paesana.jpeg`,
+      taste_scale: "10/10",
+      tags: [
+        "Dolci",
+        "Dolci al cioccolato",
+        "Torte",
+        "Ricette vegetariane",
+        "Ricette al forno",
+      ],
+    },
+  ]);
+});
 
-    app.get('/bacheca', (req, res) => {
-        res.json([
-            {
-            name: 'Ciambellone',
-            image: `http://localhost:3000/cibi/ciambellone.jpeg`,
-            taste_scale:"7/10",
-            
-            },
-            {
-            name: "cracker barbabietola",
-            image: `http://localhost:3000/cibi/cracker_barbabietola.jpeg`,
-            taste_scale:"6/10",
-            },
-            {
-            name: "pane fritto dolce",
-            image: `http://localhost:3000/cibi/pane_fritto_dolce.jpeg`,
-            taste_scale:"9/10",
-            },
-            {
-            name: "pasta barbabietola",
-            image: `http://localhost:3000/cibi/pasta_barbabietola.jpeg`,
-            taste_scale:"8/10",
-            },
-            {
-            name: "torta paesana",
-            image: `http://localhost:3000/cibi/torta_paesana.jpeg`,
-            taste_scale:"10/10",
-            }
-        ])
-    })
-
-    app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-    })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- controllers/controllerPosts.js
+++ controllers/controllerPosts.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const posts = require("../data/data");
 
 // index
 router.get("/", function (req, res) {
-  res.send("Lista Post");
+  res.json(posts);
 });
 
 // show
-router.get("/:id", function (req, res) {
-  res.send("Dettagli dei Post " + req.params.id);
+router.get("/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404);
+  }
 });
 
 // store
